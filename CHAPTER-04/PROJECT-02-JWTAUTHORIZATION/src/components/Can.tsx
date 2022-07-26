d69ff2725f2f8ed2b43f849a389ad666ce81150f
nikolas.lopes <nikolaslopes.dev@gmail.com>
2022-07-26 02:31:34 -0400

    :fire: created Can component

---
+++ CHAPTER-04/PROJECT-02-JWTAUTHORIZATION/src/components/Can.tsx
@@ -0,0 +1,20 @@
+import { ReactNode } from 'react'
+import { useCan } from '../hooks/useCan'
+
+interface CanProps {
+  children: ReactNode
+  permissions?: string[]
+  roles?: string[]
+}
+
+export function Can({ children, permissions, roles }: CanProps) {
+  const userCanSeeTheComponent = useCan({
+    permissions,
+    roles,
+  })
+
+  if (!userCanSeeTheComponent) {
+    return null
+  }
+  return <>{children}</>
+}
