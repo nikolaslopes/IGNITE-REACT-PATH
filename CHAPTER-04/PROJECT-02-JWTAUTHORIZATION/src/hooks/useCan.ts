d82b889341aa2e3265db96de508a64c1ec9b1802
nikolas.lopes <nikolaslopes.dev@gmail.com>
2022-07-26 02:06:10 -0400

    :fire: created useCan

---
+++ CHAPTER-04/PROJECT-02-JWTAUTHORIZATION/src/hooks/useCan.ts
@@ -0,0 +1,34 @@
+import { useAuthContext } from '../context/useAuthContext'
+
+type UseCanParams = {
+  permissions?: string[]
+  roles?: string[]
+}
+
+export function useCan({ permissions = [], roles = [] }: UseCanParams) {
+  const { isAuthenticated, user } = useAuthContext()
+
+  if (!isAuthenticated) {
+    return
+  }
+
+  if (permissions.length > 0) {
+    const hasAllPermissions = permissions.every((permission) => {
+      return user?.permissions.includes(permission)
+    })
+
+    if (!hasAllPermissions) {
+      return false
+    }
+  }
+
+  if (roles.length > 0) {
+    const hasAllRoles = roles.every((role) => {
+      return user?.roles.includes(role)
+    })
+
+    if (!hasAllRoles) {
+      return false
+    }
+  }
+}
