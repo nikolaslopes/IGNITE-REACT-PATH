30dd1179a149b65f390b8b3b80a40571a3fa557b
nikolas.lopes <nikolaslopes.dev@gmail.com>
2022-07-31 01:14:45 -0400

    :sparkles: add error and loading callback

--- CHAPTER-04/04-UPFI/src/pages/index.tsx
+++ CHAPTER-04/04-UPFI/src/pages/index.tsx
@@ -52,8 +52,8 @@ export default function Home(): JSX.Element {
       <Header />
 
       <Box maxW={1120} px={20} mx="auto" my={20}>
-        {/* <CardList cards={formattedData} /> */}
-        {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */}
+        <CardList cards={formattedData} />
+        TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE
       </Box>
     </>
   );
