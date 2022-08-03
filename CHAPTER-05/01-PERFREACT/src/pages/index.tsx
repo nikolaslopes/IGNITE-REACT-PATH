6e5dec5f758f27af109ebfcae07ad04c7992b015
Nikolas Lopes <nikolaslopes.dev@gmail.com>
2022-08-03 12:05:06 -0400

    :fire: created inp sty

--- CHAPTER-05/01-PERFREACT/src/pages/index.tsx
+++ CHAPTER-05/01-PERFREACT/src/pages/index.tsx
@@ -1,10 +1,22 @@
 import type { NextPage } from 'next'
+import { useState } from 'react'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
+  const [search, setSearch] = useState('')
+
   return (
     <main className={styles.main}>
-      <h1>a</h1>
+      <h1>Search</h1>
+
+      <form>
+        <input
+          className={styles.input}
+          type="text"
+          value={search}
+          onChange={(event) => setSearch(event.target.value)}
+        />
+      </form>
     </main>
   )
 }
