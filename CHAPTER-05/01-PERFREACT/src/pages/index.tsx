73b03a6f2ab0e5b19fae79207ddb513d4ed04118
Nikolas Lopes <nikolaslopes.dev@gmail.com>
2022-08-03 12:13:04 -0400

    :fire: test DS store

--- CHAPTER-05/01-PERFREACT/src/pages/index.tsx
+++ CHAPTER-05/01-PERFREACT/src/pages/index.tsx
@@ -5,17 +5,20 @@ import styles from '../styles/Home.module.css'
 const Home: NextPage = () => {
   const [search, setSearch] = useState('')
 
+  function handleSearch() {}
   return (
     <main className={styles.main}>
       <h1>Search</h1>
 
-      <form>
+      <form onSubmit={handleSearch}>
         <input
           className={styles.input}
           type="text"
           value={search}
           onChange={(event) => setSearch(event.target.value)}
         />
+
+        <button type="button">Search product</button>
       </form>
     </main>
   )
