3705a0022dd4f567202c632b2f0d0b9bb1c49729
Nikolas Lopes <nikolaslopes.dev@gmail.com>
2022-08-06 05:14:26 -0400

    :white_check_mark: add SubscribeButton tests

--- CHAPTER-03/01-IGNEWS/src/components/SubscribeButton/SubscribeButton.spec.tsx
+++ CHAPTER-03/01-IGNEWS/src/components/SubscribeButton/SubscribeButton.spec.tsx
@@ -1,21 +1,33 @@
-import { render, screen } from '@testing-library/react'
-import { useSession } from 'next-auth/react'
+import { fireEvent, render, screen } from '@testing-library/react'
+import { signIn, useSession } from 'next-auth/react'
 
 import { SubscribeButton } from '.'
 
-jest.mock('next-auth/react')
+jest.mock('next-auth/react', () => {
+  return {
+    useSession() {
+      return [null, false]
+    },
+    signIn: jest.fn(),
+  }
+})
 
 describe('SignInButton component', () => {
   it('renders correctly when user is NOT authenticated', () => {
-    const useSessionMocked = jest.mocked(useSession)
-
-    useSessionMocked.mockReturnValueOnce({
-      data: null,
-      status: 'unauthenticated',
-    })
-
     render(<SubscribeButton />)
 
     expect(screen.getByText('Subscribe now')).toBeInTheDocument()
+  })
+
+  it('redirects user to sign in when NOT authenticated', () => {
+    const signInMocked = jest.mocked(signIn)
+
+    render(<SubscribeButton />)
+
+    const subscribeButton = screen.getByText('Subscribe now')
+
+    fireEvent.click(subscribeButton)
+
+    expect(signInMocked).toHaveBeenCalled()
   })
 })
