9edd7db361004fe11029ed056cbd6d68151f7cca
Nikolas Lopes <nikolaslopes.dev@gmail.com>
2022-08-06 18:02:31 -0400

    :white_check_mark: add Home tests

--- CHAPTER-03/01-IGNEWS/src/tests/pages/Home.spec.tsx
+++ CHAPTER-03/01-IGNEWS/src/tests/pages/Home.spec.tsx
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react'
-import Home from '../../pages'
+import Home, { getStaticProps } from '../../pages'
+import { stripe } from '../../services/stripe'
 
 jest.mock('next/router')
 jest.mock('next-auth/react', () => {
@@ -9,11 +10,32 @@ jest.mock('next-auth/react', () => {
     },
   }
 })
+jest.mock('../../services/stripe')
 
 describe('Home page', () => {
   it('renders correctly', () => {
-    render(<Home product={{ amount: 'R$00' }} />)
+    render(<Home product={{ amount: '$10' }} />)
 
-    expect(screen.getByText('for R$00 month')).toBeInTheDocument()
+    expect(screen.getByText('for $10 month')).toBeInTheDocument()
+  })
+
+  it('it loads initial data', async () => {
+    const retrieveStripePricesMocked = jest.mocked(stripe.prices.retrieve)
+
+    retrieveStripePricesMocked.mockResolvedValueOnce({
+      unit_amount: 1000,
+    } as any)
+
+    const response = await getStaticProps({})
+
+    expect(response).toEqual(
+      expect.objectContaining({
+        props: {
+          product: {
+            amount: '$10.00',
+          },
+        },
+      })
+    )
   })
 })
