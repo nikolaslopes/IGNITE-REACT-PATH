0fe3b57e0a06b1d2f93849bf0bed4e0016d1c6e1
Nikolas Lopes <nikolaslopes.dev@gmail.com>
2022-08-07 03:25:00 -0400

    :white_mark_check: add post preview new tests

---
+++ CHAPTER-03/01-IGNEWS/src/tests/pages/PostPreview.spec.tsx
@@ -0,0 +1,24 @@
+import { render, screen } from '@testing-library/react'
+import { useSession } from 'next-auth/react'
+import PostPreview, { getStaticProps } from '../../pages/posts/preview/[slug]'
+import { post } from '../mocks/post'
+
+jest.mock('next-auth/react')
+jest.mock('../../services/prismic')
+
+describe('Post Preview page', () => {
+  it('renders correctly', () => {
+    const useSessionMocked = jest.mocked(useSession)
+
+    useSessionMocked.mockReturnValueOnce({
+      data: null,
+      status: 'unauthenticated',
+    })
+
+    render(<PostPreview post={post} />)
+
+    expect(screen.getByText('My New Post')).toBeInTheDocument()
+    expect(screen.getByText('Post excerpt')).toBeInTheDocument()
+    expect(screen.getByText('Wanna continue reading?')).toBeInTheDocument()
+  })
+})
--- CHAPTER-03/01-IGNEWS/src/tests/pages/Preview.spec.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react'
-import PostPreview from '../../pages/posts/preview/[slug]'
-import { post } from '../mocks/post'
-
-describe('Preview page', () => {
-  it('renders correctly', () => {
-    render(<PostPreview post={post} />)
-  })
-})
