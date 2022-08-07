5117a26746a5dde4e5b280e40010835901581cb5
Nikolas Lopes <nikolaslopes.dev@gmail.com>
2022-08-07 05:31:08 -0400

    :white_check_mark: add and passed new tests on PostPreview

--- CHAPTER-03/01-IGNEWS/src/tests/mocks/fullPost.ts
+++
@@ -1,6 +0,0 @@
-export const fullPost = {
-  slug: 'my-new-post',
-  title: 'My New Post',
-  content: '<p>Post content</p>',
-  updatedAt: '10 de Abril',
-}
--- CHAPTER-03/01-IGNEWS/src/tests/mocks/previewPost.ts
+++
@@ -1,6 +0,0 @@
-export const previewPost = {
-  slug: 'my-new-post',
-  title: 'My New Post',
-  excerpt: '<p>Post excerpt</p>',
-  updatedAt: '10 de Abril',
-}
