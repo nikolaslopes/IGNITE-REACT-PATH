d8d4561f837bf83aa4a1beeeacbf838ea11500a0
Nikolas Lopes <nikolaslopes.dev@gmail.com>
2022-08-07 03:03:25 -0400

    :white_check_mark: add and passed new Post tests

--- CHAPTER-03/01-IGNEWS/src/tests/pages/Post.spec.tsx
+++ CHAPTER-03/01-IGNEWS/src/tests/pages/Post.spec.tsx
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react'
 import { getSession } from 'next-auth/react'
 import Post, { getServerSideProps } from '../../pages/posts/[slug]'
+import { getPrismicClient } from '../../services/prismic'
 
 const post = {
   slug: 'my-new-post',
@@ -38,6 +39,53 @@ describe('Post page', () => {
         redirect: expect.objectContaining({
           destination: '/posts/preview/my-new-post',
         }),
+      })
+    )
+  })
+
+  it('loads initial data', async () => {
+    const getSessionMocked = jest.mocked(getSession)
+    const getPrismicClientMocked = jest.mocked(getPrismicClient)
+
+    getSessionMocked.mockResolvedValueOnce({
+      activeSubscription: 'fake-subscription-active',
+    } as any)
+
+    getPrismicClientMocked.mockReturnValueOnce({
+      getByUID: jest.fn().mockResolvedValueOnce({
+        uid: 'my-new-post',
+        data: {
+          title: [
+            {
+              type: 'heading',
+              text: 'My New Post',
+            },
+          ],
+          content: [
+            {
+              type: 'paragraph',
+              text: 'Post content',
+            },
+          ],
+        },
+        last_publication_date: '04-01-2022',
+      }),
+    } as any)
+
+    const response = await getServerSideProps({
+      params: { slug: 'my-new-post' },
+    } as any)
+
+    expect(response).toEqual(
+      expect.objectContaining({
+        props: {
+          post: {
+            slug: 'my-new-post',
+            title: 'My New Post',
+            content: '<p>Post content</p>',
+            updatedAt: '01 de abril de 2022',
+          },
+        },
       })
     )
   })
